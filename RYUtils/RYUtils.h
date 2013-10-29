43bfe511471e7860e3e01dad2bfd509e804da6bf
Ryan <Ryan@ryan-macpro-lan.icitymobile.com>
2013-10-29 13:13:34 +0800

    加入最新版本修改日期及修改人标识。

--- RYUtils/RYUtils.h
+++ RYUtils/RYUtils.h
@@ -31,7 +31,7 @@
     - 添加Downloader类到RYUtils中，方便外部使用对应类的delegate
  v1.7
     - 修改RYPullDownRefreshTableView类，移除类中UITableViewDatasource @required method warnings
- v1.8
+ v1.8 2013-10-29 Ryan
     - 框架支持最低修改至6.0，移除对应过期方法使用的warning
  */
 
